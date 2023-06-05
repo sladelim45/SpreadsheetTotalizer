4176a516544ed94fe414493a9986b86c9521ce18
Slade Lim <sladelim@gmail.com>
2023-06-05 16:23:34 -0600

    added css style sheet, content preview, settings

--- totalizer.js
+++ totalizer.js
@@ -1,26 +1,30 @@
 
-
+// triggered by user pressing Upload button
 function handleFileUpload(event) {
     event.preventDefault();
     console.log("called");
 
     // obtain files
     const files = document.getElementById('fileUpload').files;
-
+    
+    // make sure user actually uploaded a file
     if (files.length === 0) {
         alert('Please select at least one file.');
         return;
-      }
+    }
+    
+    // clear the spreadsheet preview
+    document.getElementById('contentPreview').innerHTML = "";
 
     // process each file, if there are multiple
-    for (let i = 0; i < files.length; i++) {
+    for (var i = 0; i < files.length; i++) {
         const file = files[i];
 
         const reader = new FileReader();
         reader.readAsText(file);
 
         reader.onload = function() {
-            processFile(file, reader.result);
+            processFile(file.name, reader.result);
         };
 
         reader.onerror = function() {
@@ -29,7 +33,40 @@ function handleFileUpload(event) {
     }
 }
 
-function processFile(file, fileContents) {
-    console.log('File: ' + file);
+
+function processFile(filename, fileContents) {
+    console.log('Filename: ' + filename);
     console.log('Contents: ' + fileContents);
+
+    // parse the TSV
+    var rows = fileContents.split('\n');
+    var data = [];
+
+    for (var i = 0; i < rows.length; i++) {
+        var cells = rows[i].split('\t');
+        data.push(cells);
+    }
+
+    displaySpreadsheet(data);
+
+
+
+}
+
+function displaySpreadsheet(data) {
+    var spreadsheet = document.createElement('table');
+
+    for (var i = 0; i < data.length; i++) {
+        var row = document.createElement('tr');
+
+        for (var j = 0; j < data[i].length; j++) {
+            var cell = document.createElement('td');
+            cell.textContent = data[i][j];
+            row.appendChild(cell);
+        }
+
+        spreadsheet.appendChild(row);
+    }
+
+    document.getElementById('contentPreview').appendChild(spreadsheet);
 }
